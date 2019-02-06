7d9bb251940ccbe0af50da236c170069187a86ad
richardcalata <calatar6@gmail.com>
2019-02-06 08:06:12 -0600

    next

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const  Heroes: Hero[] = [
+  {id: 11, name: 'Flaming Carrot'},
+  {id: 12, name: 'The Blue Raja'},
+  {id: 13, name: 'The Shoveler'},
+  {id: 14, name: 'The Bowler'},
+  {id: 15, name: 'The Spleen'},
+  {id: 16, name: "Invisible Boy"}
+];
