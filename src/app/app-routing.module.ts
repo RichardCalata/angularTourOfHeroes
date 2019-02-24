f7571ff181e0ad68dabfa453066fe9eca5294884
Richard.Calata <34409394+RichardCalata@users.noreply.github.com>
2019-02-23 19:40:34 -0600

    Test commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes} from "@angular/router";
 import { DashboardComponent } from './dashboard/dashboard.component'
 import { HeroesComponent } from "./heroes/heroes.component";
